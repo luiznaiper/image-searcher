29ae14143d6ff140df7c331c4a424edf0cbfe3a6
luiznaiper <luiz_naiper@hotmail.com>
2023-01-19 07:50:18 -0300

    :construction: Add env variables, fetch api

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Formik, Form, Field } from 'formik';
+import './css/index.css';
+import './css/header.css';
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [photos, setPhotos] = useState([]);
+  console.log({ photos });
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <header>
+        <Formik
+          initialValues={{ search: '' }}
+          onSubmit={async (values) => {
+            const response = await fetch(
+              `https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`,
+              {
+                headers: {
+                  Authorization: `Client-ID ${import.meta.env.VITE_CLIENT_ID}`,
+                },
+              }
+            );
+            const data = await response.json();
+            setPhotos(data.results);
+          }}
+        >
+          <Form>
+            <Field name="search" />
+          </Form>
+        </Formik>
+      </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
